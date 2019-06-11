5bd7b5bfc1a101def68c0e4cd42fad820888276d
Dom Steil <dominic@dapps-inc.com>
2019-06-11 13:41:04 -0700

    adding corbeans webserver

--- contracts/src/main/kotlin/io/baton/states/UPIState.kt
+++ contracts/src/main/kotlin/io/baton/states/UPIState.kt
@@ -1,5 +1,5 @@
 /**
- *   Copyright 2019, Dapps Incorporated.
+ *   Copyright 2020, Dapps Incorporated.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
